4361af5e4477df41e3b69af079ad7098daae8979
SeungHyun <fm10033@gmail.com>
2021-09-23 15:31:50 +0900

    redux 프로젝트 준비

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import CounterContainer from "./containers/CounterContainer";
+
+function App() {
+  return <CounterContainer></CounterContainer>;
+}
+
+export default App;
---
+++ src/modules/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import counter from "./counter";
+
+const Rootreducer = combineReducers({ counter });
+
+export default Rootreducer;
