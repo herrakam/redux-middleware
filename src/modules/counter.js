79d2257033a4b3d1fd90cfbadfb243b417a8f275
SeungHyun <fm10033@gmail.com>
2021-09-23 18:07:53 +0900

    redux-thunk 설치, 간단한 사용

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -4,6 +4,18 @@ const DECREASE = "DECREASE";
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
 
+export const increaseAsync = () => (dispatch) => {
+  setTimeout(() => {
+    dispatch(increase());
+  }, 1000);
+};
+
+export const decreaseAsync = () => (dispatch) => {
+  setTimeout(() => {
+    dispatch(decrease());
+  }, 1000);
+};
+
 const initialState = 0;
 
 export default function counter(state = initialState, action) {
@@ -12,6 +24,7 @@ export default function counter(state = initialState, action) {
       return state + 1;
     case DECREASE:
       return state - 1;
+
     default:
       return state;
   }
