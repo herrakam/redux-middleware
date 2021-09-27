810c1f38c138c2eb0c5b5cbd81f220d7a252cce3
SeungHyun <fm10033@gmail.com>
2021-09-27 18:57:05 +0900

    포스트 데이터 최적화, 리펙토링 필요

--- src/containers/PostContainer.js
+++ src/containers/PostContainer.js
@@ -1,20 +1,21 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Post from "../components/Post";
+import { reducerUtils } from "../lib/asyncUtils";
 import { clearPost, getPost } from "../modules/posts";
 
 function PostContainer({ postId }) {
-  const { data, loading, error } = useSelector((state) => state.posts.post);
+  const { data, loading, error } = useSelector(
+    (state) => state.posts.post[postId] || reducerUtils.initial()
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
+    if (data) return;
     dispatch(getPost(postId));
-    return () => {
-      dispatch(clearPost());
-    };
-  }, [postId, dispatch]);
+  }, [postId, dispatch, data]);
 
-  if (loading) return <div> 로딩중</div>;
+  if (loading && !data) return <div> 로딩중</div>;
   if (error) return <div>에러발생</div>;
   if (!data) return null;
 
