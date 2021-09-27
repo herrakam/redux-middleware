c18619400fc5a437a0dc85220ad633413b35b9d5
SeungHyun <fm10033@gmail.com>
2021-09-27 15:28:41 +0900

    PostList 구현

---
+++ src/containers/PostListContainer.js
@@ -0,0 +1,42 @@
+// import React, { useEffect } from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import PostList from "../components/PostList";
+// import { getPosts } from "../modules/posts";
+
+// function PostListContainer() {
+//   const { data, loading, error } = useSelector((state) => state.posts.posts);
+//   const dispatch = useDispatch();
+//   useEffect(() => {
+//     dispatch(getPosts());
+//   }, [dispatch]); // 규칙상 넣었음, dispatch 안넣어도 작동에는 영향 x
+
+//   if (loading) return <div>로딩중...</div>;
+//   if (error) return <div>에러 발생</div>;
+//   if (!data) return null;
+
+//   return <PostList posts={data}></PostList>;
+// }
+
+// export default PostListContainer;
+
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import PostList from "../components/PostList";
+import { getPosts } from "../modules/posts";
+
+function PostListContainer() {
+  const { data, loading, error } = useSelector((state) => state.posts.posts);
+  const dispatch = useDispatch();
+
+  // 컴포넌트 마운트 후 포스트 목록 요청
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러 발생!</div>;
+  if (!data) return null;
+  return <PostList posts={data} />;
+}
+
+export default PostListContainer;
