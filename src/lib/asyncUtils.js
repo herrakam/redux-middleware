6b4870e1f7e5ae6f1c8385a4401a0da3311475a5
SeungHyun <fm10033@gmail.com>
2021-09-30 16:04:53 +0900

    createPromiseThunkById, handleAsyncActionsById로 리팩토링 완료

--- src/lib/asyncUtils.js
+++ src/lib/asyncUtils.js
@@ -68,3 +68,70 @@ export const createPromiseThunk = (type, promiseCreator) => {
   };
   return thunkCreator;
 };
+const defaultIdSelector = (param) => param;
+
+export const createPromiseThunkById = (
+  type,
+  promiseCreator,
+  idSelector = defaultIdSelector
+) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+  return (param) => async (dispatch) => {
+    const id = idSelector(param);
+    dispatch({ type, meta: id });
+    try {
+      const payload = await promiseCreator(param);
+      dispatch({
+        type: SUCCESS,
+        payload, //payLoad: 모든 액션의 추가값은 payload로 통일시킴(flux standard action 검색!)
+        meta: id,
+      });
+    } catch (e) {
+      dispatch({
+        type: ERROR,
+        payload: e,
+        error: true, //에러발생시 error:true로 설정해야함(fsa)
+        meta: id,
+      });
+    }
+  };
+};
+
+export const hanldeAsyncActonsById = (type, key, keepData) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+  return (state, action) => {
+    const id = action.meta;
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: {
+            ...state[key],
+            [id]: reducerUtils.loading(
+              keepData ? state[key][id] && state[key][id].data : null
+            ),
+          },
+        };
+      case SUCCESS: {
+        return {
+          ...state,
+          [key]: {
+            ...state[key],
+            [id]: reducerUtils.success(action.payload),
+          },
+        };
+      }
+      case ERROR:
+        return {
+          ...state,
+          [key]: {
+            ...state[key],
+            [id]: reducerUtils.error(action.payload),
+          },
+        };
+      default:
+        return state;
+    }
+  };
+};
--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,7 +1,9 @@
 import * as postsAPI from "../api/posts";
 import {
   createPromiseThunk,
+  createPromiseThunkById,
   hanldeAsyncActons,
+  hanldeAsyncActonsById,
   reducerUtils,
 } from "../lib/asyncUtils";
 
@@ -15,20 +17,7 @@ const GET_POST_ERROR = "GET_POST_ERROR";
 const CLEAR_POST = "CLEAR_POST";
 
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = (id) => async (dispatch) => {
-  dispatch({ type: GET_POST, meta: id });
-  try {
-    const payload = await postsAPI.getPostById(id);
-    dispatch({ type: GET_POST_SUCCESS, payload, meta: id });
-  } catch (e) {
-    dispatch({
-      type: GET_POST_ERROR,
-      payload: e,
-      error: true,
-      meta: id,
-    });
-  }
-};
+export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
 export const clearPost = () => ({
   type: CLEAR_POST,
 });
@@ -39,37 +28,7 @@ const initialState = {
 };
 
 const getPostsReducer = hanldeAsyncActons(GET_POSTS, "posts", true);
-const getPostReducer = (state, action) => {
-  const id = action.meta;
-  switch (action.type) {
-    case GET_POST:
-      return {
-        ...state,
-        post: {
-          ...state.post,
-          [id]: reducerUtils.loading(state.post[id] && state.post[id].data),
-        },
-      };
-    case GET_POST_SUCCESS:
-      return {
-        ...state,
-        post: {
-          ...state.post,
-          [id]: reducerUtils.success(action.payload),
-        },
-      };
-    case GET_POST_ERROR:
-      return {
-        ...state,
-        post: {
-          ...state.post,
-          [id]: reducerUtils.error(action.payload),
-        },
-      };
-    default:
-      return state;
-  }
-};
+const getPostReducer = hanldeAsyncActonsById(GET_POST, "post", true);
 export default function posts(state = initialState, action) {
   switch (action.type) {
     case GET_POSTS:
