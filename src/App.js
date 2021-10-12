dd538c36ba841c56255b07190c8e2b12c2e351f7
SeungHyun <fm10033@gmail.com>
2021-10-12 17:10:00 +0900

    redux-saga로 increaseAsync,decreaseAsync 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import CounterContainer from "./containers/CounterContainer";
-import PostListContainer from "./containers/PostListContainer";
 import { Route } from "react-router-dom";
 import PostListPage from "./pages/PostListPage";
 import PostPage from "./pages/PostPage";
 function App() {
   return (
     <>
+      <CounterContainer></CounterContainer>
       <Route path="/" component={PostListPage} exact={true}></Route>
       <Route path="/:id" component={PostPage}></Route>
     </>
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
-import counter from "./counter";
+import counter, { counterSaga } from "./counter";
 import posts from "./posts";
+import { all } from "redux-saga/effects";
 const Rootreducer = combineReducers({ counter, posts });
-
+export function* rootSaga() {
+  yield all([counterSaga()]);
+}
 export default Rootreducer;
